2082fab3488f29ed34591d2d241ae5462452ab12
ArtChr <artchrr@gmail.com>
2019-03-03 19:06:29 +0200

    hw2-adv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,65 @@ import { IHotel } from './hotel';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public title: string = 'hw2';
+  public title: string = 'hw2-adv';
 
-  public hotel: IHotel = {
-    id: 0,
-    title: 'Universal Cabana',
-    address: 'Orlando',
-    description: 'Best one!',
-    phone: '+3242353434',
-    picture: 'assets/images/1.jpg',
-    photos: [
-      'assets/images/res.jpg',
-      'assets/images/r1.jpg'
-    ],
-    weather: {
-      temperature: 12,
-      water: 11,
-      icon: 'sun'
+  public selectedHotel: IHotel;
+
+  public hotels: IHotel[] = [
+    {
+      id: 0,
+      title: 'Universal Cabana',
+      address: 'Orlando',
+      description: 'Best one!',
+      phone: '+3242353434',
+      picture: 'assets/images/1.jpg',
+      photos: [
+        'assets/images/res.jpg',
+        'assets/images/r1.jpg'
+      ],
+      weather: {
+        temperature: 12,
+        water: 11,
+        icon: 'sun'
+      },
+      profile: {
+        followers: 112,
+        following: 11,
+        photo: 'assets/images/b1.jpg'
+      },
+      stars: 3
     },
-    profile: {
-      followers: 112,
-      following: 11,
-      photo: 'assets/images/b1.jpg'
-    },
-    stars: 3
+    {
+      id: 1,
+      title: 'Cabana #2',
+      address: 'Florida',
+      description: 'Good price',
+      phone: '+3123456789',
+      picture: 'assets/images/res.jpg',
+      photos: [
+        'assets/images/res.jpg',
+        'assets/images/r1.jpg'
+      ],
+      weather: {
+        temperature: 20,
+        water: 17,
+        icon: 'sun'
+      },
+      profile: {
+        followers: 574,
+        following: 164,
+        photo: 'assets/images/r1.jpg'
+      },
+      stars: 4
+    }
+  ];
+
+  ngOnInit(): void {
+    this.selectedHotel = this.hotels[0];
+  }
+
+
+  public onRowClick(i: number): void {
+    this.selectedHotel = this.hotels[i];
   }
 }
